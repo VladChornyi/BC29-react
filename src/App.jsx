1985242db7d5772a6d6a9175aa774bff4cf64602
AndreiKrai <okraichenko.a@gmail.com>
2022-10-17 15:50:39 +0300

    add list

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import { PostList } from "./components/PostList";
 
 function App() {
+  const myTitle= 'Name'
   return (
     <>
       <h2 className="header-title">Котик на диете - несчастный котик</h2>
-      <PostList />
+      <PostList title={myTitle}/>
     </>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,15 +1,15 @@
 import data from "../data/data.json";
-export const PostList = () => {
-  console.log("data", data);
+export const PostList = ({title}) => {
+  // console.log("props", props);
   return (
     <>
-      <h2 className="post-list-title">Post List</h2>
+      {title && <h2 className="post-list-title">{title}</h2>}
       <ul className="post-list">
-        <li className="post-list-item">
-          <img className="post-list-img" src={data[0].image} alt="" />
-          <h4 className="post-list-item-title">{data[0].title}</h4>
-          <p className="post-list-item-text">{data[0].description}</p>
-        </li>
+        {data.map(item=>(<li className="post-list-item" key={item.id}>
+          <img className="post-list-img" src={item.image} alt="" />
+          <h4 className="post-list-item-title">{item.title}</h4>
+          <p className="post-list-item-text">{item.description}</p>
+        </li>))}
       </ul>
     </>
   );
