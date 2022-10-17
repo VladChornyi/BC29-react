96833a4bfa0b9f0737f6aa67097b906e9dd283c2
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-10-17 15:13:45 +0300

    adds solution

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,14 @@
+import data from "../data/data.json";
 export const PostList = () => {
+  console.log("data", data);
   return (
     <>
-      <h2 class="post-list-title">Post List</h2>
-      <ul class="post-list">
-        <li class="post-list-item">
-          <img class="post-list-img" src="" alt="" />
-          <h4 class="post-list-item-title">Post title</h4>
-          <p class="post-list-item-text">Post text</p>
+      <h2 className="post-list-title">Post List</h2>
+      <ul className="post-list">
+        <li className="post-list-item">
+          <img className="post-list-img" src={data[0].image} alt="" />
+          <h4 className="post-list-item-title">{data[0].title}</h4>
+          <p className="post-list-item-text">{data[0].description}</p>
         </li>
       </ul>
     </>
