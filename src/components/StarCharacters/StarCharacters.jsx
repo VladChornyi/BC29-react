dce908c85e4acef9d9f5371ede8755213f9347eb
yuliia-andriitsova <andriitsovayulia@gmail.com>
2022-10-25 17:05:13 +0300

    modal

--- src/components/StarCharacters/StarCharacters.jsx
+++ src/components/StarCharacters/StarCharacters.jsx
@@ -1,27 +1,27 @@
-import PropTypes from 'prop-types'
-import { Component } from 'react'
-import { getCharacters } from '../../helpers/api'
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+// import { getCharacters } from '../../helpers/api';
 
 export default class StarCharacters extends Component {
-  state = {
-  characters: [],
-  }
-  
-async componentDidUpdate(prevProps, prevState) {
-    if (prevProps.catchName !== this.props.catchName) {
-      const data = await getCharacters(this.props.catchName)
-      this.setState({characters: data.results})
-   }
- }
-
   render() {
     return (
-      <ul></ul>
-    )
+      <ul>
+        {this.props.characters.map((character) => (
+          <li
+            key={character.name}
+            onClick={this.props.toggleModal}
+            id={character.name}
+          >
+            <span> name {character.name}</span>
+            <span> gender {character.gender}</span>
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
 StarCharacters.propTypes = {
   catchName: PropTypes.string.isRequired,
-}
-
+  toggleModal: PropTypes.func.isRequired,
+};
--- src/components/Starwars/Starwars.jsx
+++ src/components/Starwars/Starwars.jsx
@@ -1,23 +1,61 @@
 // import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import StarCharacters from '../StarCharacters/StarCharacters'
-import StarForm from '../StarForm/StarForm'
-
+import React, { Component } from 'react';
+import { getCharacters } from '../../helpers/api';
+import StarCharacters from '../StarCharacters/StarCharacters';
+import StarForm from '../StarForm/StarForm';
+import StarModal from '../StarModal/StarModal';
 
 export class Starwars extends Component {
-state = {
-  catchName: '',
-}
-  
-  handleSubmit = catchName => {
-  this.setState({catchName})
-}  
+  state = {
+    catchName: '',
+    isOpenModal: false,
+    characters: [],
+    activeName: '',
+  };
+
+  handleSubmit = (catchName) => {
+    this.setState({ catchName });
+  };
+
+  toggleModal = (event) => {
+    const name = event.currentTarget.id;
+
+    if (name) {
+      this.setState({ activeName: name });
+    }
+    this.setState((prevState) => ({
+      isOpenModal: !prevState.isOpenModal,
+    }));
+  };
+
+  getActiveData = () => {
+    console.log(this.state.activeName);
+    return this.state.characters.find(
+      (character) => this.state.activeName === character.name
+    );
+  };
+  async componentDidUpdate(_, prevState) {
+    if (prevState.catchName !== this.state.catchName) {
+      const data = await getCharacters(this.state.catchName);
+      this.setState({ characters: data.results });
+    }
+  }
+
   render() {
     return (
       <>
-      <StarForm onSubmit={this.handleSubmit} />
-        <StarCharacters catchName={this.state.catchName} />
+        <StarForm onSubmit={this.handleSubmit} />
+        <StarCharacters
+          characters={this.state.characters}
+          toggleModal={this.toggleModal}
+        />
+        {this.state.isOpenModal && (
+          <StarModal
+            toggleModal={this.toggleModal}
+            starData={this.getActiveData()}
+          />
+        )}
       </>
-    )
+    );
   }
 }
