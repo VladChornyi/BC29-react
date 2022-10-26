d152fc1f46ba1815ab28691610a32a04089cd9df
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-10-26 15:39:39 +0300

    add huks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,12 @@ import "./App.css";
 import actorsData from "../data/acter.json";
 import { Actors } from "../components/ActerList/Acter.jsx";
 import { ProductsList } from "./ProductsList";
-import { Tabs } from './Tabs/Tabs';
+import { Tabs } from "./Tabs/Tabs";
 import { Section } from "./Section/Section";
-import description from '../data/description.json';
+import description from "../data/description.json";
 // import { Starwars } from "./Starwars/Starwars";
-import {Albums} from "./Albums/Albums"
+import { Albums } from "./Albums/Albums";
 import { useState } from "react";
-
 
 const App = () => {
   // state = {
@@ -22,48 +21,45 @@ const App = () => {
 
   const handleBold = (e) => {
     // this.setState({ activeEl: e.target.id });
-    setActiveEl(e.target.id) 
+    setActiveEl(e.target.id);
   };
 
-
   const actorDelete = (event) => {
-  
     // this.setState((prevState) => {
     //   return {
     //     actors: prevState.actors.filter((actor) => actor.id != event.target.id),
     //   };
     // });
-    // setActors(prevActors => prevActors.filter((actor) => actor.id != event.target.id)) 
-    setActors(actors.filter((actor) => actor.id != event.target.id)) 
+    // setActors(prevActors => prevActors.filter((actor) => actor.id != event.target.id))
+    setActors(actors.filter((actor) => actor.id != event.target.id));
   };
 
-  
-    return (<>
-        <Section title={"Actors"}>
-          <Actors actors={actors} actorDelete={actorDelete} />
-        </Section>
+  return (
+    <>
+      <Section title={"Actors"}>
+        <Actors actors={actors} actorDelete={actorDelete} />
+      </Section>
 
-        {/* <Section title={"Products"}>
+      <Section title={"Products"}>
         <ProductsList
-          actors={this.state.actors}
-          activeEl={this.state.activeEl}
-          handleBold={this.handleBold}
-          />
-        </Section>
+          actors={actorsData}
+          activeEl={activeEl}
+          handleBold={handleBold}
+        />
+      </Section>
 
-        <Section title={"Tabs"}>
-          <Tabs description={description} />
-        </Section>
+      <Section title={"Tabs"}>
+        <Tabs description={description} />
+      </Section>
 
-
-        <Section title={"Starwars"}>
+      {/* <Section title={"Starwars"}>
         <Starwars />
         </Section>
         <Section title={"Albums"}>
           <Albums />
         </Section> */}
-      </>
-    );
-}
+    </>
+  );
+};
 
 export default App;
