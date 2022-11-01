b12e47ddb4e7b2b57f0a04fb94533d80763f71c1
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-11-01 16:33:01 +0200

    adds task solution

--- src/pages/Cats/Cats.jsx
+++ src/pages/Cats/Cats.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { getCatBreed, getCatData } from '../../helpers/api';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { getCatBreed, getCatData } from "../../helpers/api";
 
 export function Cats() {
   const [breeds, setBreeds] = useState([]);
-  const [active, setActive] = useState('');
+  const [activeId, setActiveId] = useState("");
 
   useEffect(() => {
     // getCatData();
@@ -17,24 +18,32 @@ export function Cats() {
     getBreeds();
   }, []);
 
-  const getActive = (e) => {
-    setActive(e.target.value);
+  const getActiveId = (e) => {
+    setActiveId(e.target.value);
   };
 
-  const getImageActive = () =>
-    breeds.find((breed) => breed.id === active)?.image.url;
+  const getImageActive = () => breeds.find((breed) => breed.id === activeId);
 
   return (
-    <div>
-      Cats
-      <select name="breeds" onChange={getActive}>
-        {breeds?.map(({ id, name }) => (
-          <option key={id} value={id}>
-            {name}
-          </option>
-        ))}
-      </select>
-      <img src={getImageActive()} width="500" alt="cat" />
-    </div>
+    <>
+      <div>
+        Cats
+        <select name="breeds" onChange={getActiveId}>
+          {breeds?.map(({ id, name }) => (
+            <option key={id} value={id}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <img src={getImageActive()?.image.url} width="500" alt="cat" />
+      </div>
+      <div>
+        {activeId && (
+          <Link to={`/cats/${activeId}`}>
+            Show more {getImageActive()?.name}
+          </Link>
+        )}
+      </div>
+    </>
   );
 }
