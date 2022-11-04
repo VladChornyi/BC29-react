4b3e0581ae129bdaefa1253e71b12c36a46db859
Makievskiy Dmytro <dimamakievskii@gmail.com>
2022-11-04 15:58:07 +0200

    tabs redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import actorsReducer from './products/slice.productList';
+import tabsReducer from './tabs/sliceTabs';
 
 export const rootReducer = combineReducers({
   actors: actorsReducer,
+  tabs: tabsReducer,
 });
